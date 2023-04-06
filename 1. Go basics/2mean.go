207e100167bffc4e244bc228d83b72d2f67ce4f4
Arvind Singharpuria <65646063+Arvind644@users.noreply.github.com>
2023-04-06 22:54:45 +0530

    numbers and assignments

---
+++ 1. Go basics/2mean.go
@@ -0,0 +1,42 @@
+// Calculate the mean of two numbers
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	var y int
+
+	x = 1
+	y = 2
+
+	/*
+		var x float64
+		var y float64
+		x = 1.0
+		y = 2.0
+	*/
+
+	/*
+		x := 1.0
+		y := 2.0
+	*/
+
+	/*
+		x, y := 1.0, 2.0
+	*/
+
+	fmt.Printf("x=%v, type of %T\n", x, x)
+	fmt.Printf("y=%v, type of %T\n", y, y)
+
+	mean := (x + y) / 2.0
+	fmt.Printf("result: %v, type of %T\n", mean, mean)
+}
+
+// float32, float 64
+/* int, uint
+int8, int16, int32, int64
+uint8, uint16, uint32, uint64
+8/
